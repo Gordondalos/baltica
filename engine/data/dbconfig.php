e8ec7b1016c731d6ccdeac12391ce524577005f2
gordondalos <gordondalos@gmail.com>
2015-11-10 17:05:19 +0600

    Настроил добавил свой шаблон

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "baltica");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "dle");
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8mb4");
+
+define('SECURE_AUTH_KEY', '7O`- Vbsiv~`}6:/]/< `U:SY,(,E]NdtP?t!J@`j`[-]gp}ml])!J51f@?|gd,');
+
+$db = new db;
+
+?>
